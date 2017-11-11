2f1f79cddcffd4c9f789b7ce8884326b0af6936f
CubetaGato <cubetagato@gmail.com>
2017-11-10 23:40:44 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const config = require('./lib/configuration');
+const log = require('./lib/utils/logger');
+const app = require('./lib/utils/server');
+//const db = require('./lib/utils/db');
+
+(function main () {
+
+  var server = app.createServer();
+
+  server.listen(config.get('server:port'), config.get('server:host'), function () {
+    log.info('Listening at ' + server.url);
+  });
+})();
